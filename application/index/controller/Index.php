0d7697eaa2a4cb749ef606a1c399bf8e1c0538ea
LGX <jiaozilgx@163.com>
2017-12-03 20:42:58 +0800

    subscribe

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,11 +18,64 @@ class Index extends Controller
         $tmp = implode('',$array);
         $tmp = sha1($tmp);
 
-        if($tmp == $signature){
+        if($tmp == $signature&&$$_GET['echostr']){
             echo $_GET['echostr'];
             exit;
+        } else {
+            $this->reposenMsg();
         }
     }
+
+    public function reposenMsg()
+    {
+        // 获取微信推送过来的post数据(xml格式)
+        $postInfo = $GLOBALS['HTTP_RAW_POST_DATA'];
+        // 将XML字符串解释为对象
+        $postObj = simplexml_load_string($postInfo);
+        // 判断是否为推送事件
+        if(strtolower($postObj->MsgType) == 'event'){
+            // 判断是否为关注
+            if(strtolower($postObj->Event) == 'subscribe'){
+                $template = '<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                            </xml>';
+                $toUser = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $createTime = time();
+                $msgType = 'text';
+                $content = 'Welcome to myWeChat'.$postObj->ToUserName;
+                // 返回一个格式化字符串
+                echo sprintf($template,$toUser,$fromUser,$createTime,$msgType,$content);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public function show()
     {
